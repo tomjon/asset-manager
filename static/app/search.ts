a9e06360ea978f8d6c93ff297b20ec3621fff2f4
Tom Winch <tom.winch@ofcom.org.uk>
2016-12-16 21:09:23 +0000

    Add missing file

---
+++ static/app/search.ts
@@ -0,0 +1,5 @@
+import { PAGE_SIZE } from './results';
+
+export class Search {
+  constructor(public text: string="*", public start: number=0, public rows: number=PAGE_SIZE, public filters: any[]=[], public order: any={}, public facets: string[]=[]) {}
+}
