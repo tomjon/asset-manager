06b4b1e76f051f3c48be106d835b5e761a6b59e0
Tom Winch <tom.winch@ofcom.org.uk>
2017-03-01 09:12:35 +0000

    Issue #44 - make barcode searchable on table view

--- solr/delete_all.sh
+++ solr/delete_all.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl http://localhost:8983/solr/$1/update?commit=true -H "Content-Type: text/xml" --data-binary '<delete><query>*:*</query></delete>'
+if [ "" == "$1" ]; then
+    echo "Usage: $0 <SOLR collection>"
+    exit 1
+fi
+
+curl "http://localhost:8983/solr/$1/update?commit=true" -H "Content-Type: text/xml" --data-binary '<delete><query>*:*</query></delete>'
