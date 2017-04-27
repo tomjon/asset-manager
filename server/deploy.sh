e6e70c83b983e5dcdb40490bea2ee7a38cb9e36c
Tom Winch <tom.winch@ofcom.org.uk>
2017-04-27 13:52:51 +0100

    Add import TPR script to deployy

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,4 +3,4 @@ if [ "" = "$1" ]; then
   echo "e.g. $0 tom@bart.ofcom.net"
   exit 1
 fi
-scp config.py.example logger.py notifications.py solr.py server.py sql.py sql_app.py user_app.py xjoin.py $1:/usr/lib/badass/server
+scp import_tprs.py config.py.example logger.py notifications.py solr.py server.py sql.py sql_app.py user_app.py xjoin.py $1:/usr/lib/badass/server
