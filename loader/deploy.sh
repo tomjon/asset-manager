86c6fbfbcb9a4bac3910086a2d5ddcf5aff343f7
Tom Winch <tom.winch@ofcom.org.uk>
2017-06-26 11:00:09 +0100

    Update deploy script

--- loader/deploy.sh
+++ loader/deploy.sh
@@ -1,1 +1,6 @@
-scp ../field_map alphabet.py attachments.py field_map.py function.py loader.py parser.py $1:/usr/lib/badass/loader
+if [ "" = "$1" ]; then
+  echo "Usage: $0 [user@host]"
+  echo "e.g. $0 tom@bart.ofcom.net"
+  exit 1
+fi
+scp ../field_map attachments.py field_map.py function.py loader.py parser.py $1:/usr/lib/badass/loader
