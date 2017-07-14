75d67d5a153ffb1a08549099847be405f40c6f21
Tom Winch <tom.winch@ofcom.org.uk>
2017-07-14 12:57:25 +0100

    Include import_projects.py in deploy

--- loader/deploy.sh
+++ loader/deploy.sh
@@ -3,4 +3,4 @@ if [ "" = "$1" ]; then
   echo "e.g. $0 tom@bart.ofcom.net"
   exit 1
 fi
-scp ../field_map attachments.py field_map.py function.py loader.py parser.py $1:/usr/lib/badass/loader
+scp ../field_map attachments.py field_map.py function.py loader.py parser.py import_projects.py $1:/usr/lib/badass/loader
